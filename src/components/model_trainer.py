67ba24f341250e155c429f25624e55cae8066acd
SamsClan <samidhapatel23.com>
2026-05-08 06:07:18 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -12,7 +12,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from xgboost import XGBRegressor
+# from xgboost import XGBRegressor  # Skipping due to missing OpenMP on macOS
 
 from src.exception import CustomException
 from src.logger import logging
@@ -43,12 +43,40 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-Neighbours Classifier": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
                 "CatBoosting Classifier": CatBoostRegressor(verbose=False),
                 "AdaBoost Classifier": AdaBoostRegressor(),
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                
+                "K-Neighbours Classifier":{
+                    'n_neighbors':[5,7,9,11],
+                
+                },
+                "CatBoosting Classifier":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations': [30,50,100]
+                },
+                "AdaBoost Classifier":{
+                    'learning_rate': [0.1,0.01,0.5,0.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             #To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
